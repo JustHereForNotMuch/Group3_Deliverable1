0db732ca569f211daa159b7b10258c188042d8f6
Luqmaan <u26265193@tuks.co.za>
2026-08-06 20:23:53 +0200

    Add files via upload

--- Group3_Deliverable1/SignUp.Designer.cs
+++ Group3_Deliverable1/SignUp.Designer.cs
@@ -42,13 +42,13 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnAccCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAccCreate.Location = new System.Drawing.Point(570, 483);
-            this.btnAccCreate.Margin = new System.Windows.Forms.Padding(6);
+            this.btnAccCreate.Location = new System.Drawing.Point(311, 262);
             this.btnAccCreate.Name = "btnAccCreate";
-            this.btnAccCreate.Size = new System.Drawing.Size(350, 55);
+            this.btnAccCreate.Size = new System.Drawing.Size(191, 30);
             this.btnAccCreate.TabIndex = 11;
             this.btnAccCreate.Text = "Create account";
             this.btnAccCreate.UseVisualStyleBackColor = true;
+            this.btnAccCreate.Click += new System.EventHandler(this.btnAccCreate_Click);
             // 
             // btnSignIn
             // 
@@ -56,8 +56,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSignIn.Location = new System.Drawing.Point(650, 497);
-            this.btnSignIn.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSignIn.Location = new System.Drawing.Point(355, 269);
             this.btnSignIn.Name = "btnSignIn";
             this.btnSignIn.Size = new System.Drawing.Size(0, 0);
             this.btnSignIn.TabIndex = 10;
@@ -71,10 +70,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblPassword.AutoSize = true;
             this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPassword.Location = new System.Drawing.Point(639, 382);
-            this.lblPassword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPassword.Location = new System.Drawing.Point(349, 207);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(183, 42);
+            this.lblPassword.Size = new System.Drawing.Size(106, 25);
             this.lblPassword.TabIndex = 9;
             this.lblPassword.Text = "Password";
             // 
@@ -85,10 +83,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblUsername.AutoSize = true;
             this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.Location = new System.Drawing.Point(639, 290);
-            this.lblUsername.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblUsername.Location = new System.Drawing.Point(349, 157);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(190, 42);
+            this.lblUsername.Size = new System.Drawing.Size(110, 25);
             this.lblUsername.TabIndex = 8;
             this.lblUsername.Text = "Username";
             // 
@@ -97,10 +94,9 @@
             this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPassword.Location = new System.Drawing.Point(511, 429);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(6);
+            this.txtPassword.Location = new System.Drawing.Point(279, 232);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(454, 29);
+            this.txtPassword.Size = new System.Drawing.Size(249, 20);
             this.txtPassword.TabIndex = 7;
             // 
             // txtUsername
@@ -108,23 +104,23 @@
             this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtUsername.Location = new System.Drawing.Point(511, 336);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUsername.Location = new System.Drawing.Point(279, 182);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(454, 29);
+            this.txtUsername.Size = new System.Drawing.Size(249, 20);
             this.txtUsername.TabIndex = 6;
             // 
             // SignUp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1436, 851);
+            this.ClientSize = new System.Drawing.Size(783, 461);
             this.Controls.Add(this.btnAccCreate);
             this.Controls.Add(this.btnSignIn);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblUsername);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUsername);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SignUp";
             this.Text = "SignUp";
             this.ResumeLayout(false);
