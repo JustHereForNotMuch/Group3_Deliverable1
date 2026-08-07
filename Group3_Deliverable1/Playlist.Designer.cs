7a38edafba55a730d7c8efe5fe566fc4c76ece37
Juan de Jager <u26350352@up.ac.za>
2026-08-07 16:05:30 +0200

    Playlist created and code entered. Debugging pending.

--- Group3_Deliverable1/Playlist.Designer.cs
+++ Group3_Deliverable1/Playlist.Designer.cs
@@ -28,20 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Playlist));
+            this.lstSongs = new System.Windows.Forms.ListBox();
+            this.lblPlaylistTitle = new System.Windows.Forms.Label();
+            this.btnAddSong = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnPlaySong = new System.Windows.Forms.Button();
+            this.btnDeleteSong = new System.Windows.Forms.Button();
+            this.btnDeletePlaylist = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lstSongs
+            // 
+            this.lstSongs.FormattingEnabled = true;
+            this.lstSongs.Location = new System.Drawing.Point(170, 100);
+            this.lstSongs.Name = "lstSongs";
+            this.lstSongs.Size = new System.Drawing.Size(284, 134);
+            this.lstSongs.TabIndex = 0;
+            // 
+            // lblPlaylistTitle
+            // 
+            this.lblPlaylistTitle.AutoSize = true;
+            this.lblPlaylistTitle.Location = new System.Drawing.Point(226, 50);
+            this.lblPlaylistTitle.Name = "lblPlaylistTitle";
+            this.lblPlaylistTitle.Size = new System.Drawing.Size(35, 13);
+            this.lblPlaylistTitle.TabIndex = 1;
+            this.lblPlaylistTitle.Text = "label1";
+            // 
+            // btnAddSong
+            // 
+            this.btnAddSong.Location = new System.Drawing.Point(170, 267);
+            this.btnAddSong.Name = "btnAddSong";
+            this.btnAddSong.Size = new System.Drawing.Size(75, 23);
+            this.btnAddSong.TabIndex = 2;
+            this.btnAddSong.Text = "Add Song";
+            this.btnAddSong.UseVisualStyleBackColor = true;
+            this.btnAddSong.Click += new System.EventHandler(this.btnAddSong_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "ofd";
+            // 
+            // btnPlaySong
+            // 
+            this.btnPlaySong.Location = new System.Drawing.Point(299, 266);
+            this.btnPlaySong.Name = "btnPlaySong";
+            this.btnPlaySong.Size = new System.Drawing.Size(75, 23);
+            this.btnPlaySong.TabIndex = 3;
+            this.btnPlaySong.Text = "Play song";
+            this.btnPlaySong.UseVisualStyleBackColor = true;
+            this.btnPlaySong.Click += new System.EventHandler(this.btnPlaySong_Click);
+            // 
+            // btnDeleteSong
+            // 
+            this.btnDeleteSong.Location = new System.Drawing.Point(422, 266);
+            this.btnDeleteSong.Name = "btnDeleteSong";
+            this.btnDeleteSong.Size = new System.Drawing.Size(138, 23);
+            this.btnDeleteSong.TabIndex = 4;
+            this.btnDeleteSong.Text = "Remove song";
+            this.btnDeleteSong.UseVisualStyleBackColor = true;
+            this.btnDeleteSong.Click += new System.EventHandler(this.btnDeleteSong_Click);
+            // 
+            // btnDeletePlaylist
+            // 
+            this.btnDeletePlaylist.Location = new System.Drawing.Point(619, 265);
+            this.btnDeletePlaylist.Name = "btnDeletePlaylist";
+            this.btnDeletePlaylist.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletePlaylist.TabIndex = 5;
+            this.btnDeletePlaylist.Text = "Delete Playlist";
+            this.btnDeletePlaylist.UseVisualStyleBackColor = true;
+            this.btnDeletePlaylist.Click += new System.EventHandler(this.btnDeletePlaylist_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(271, 328);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(310, 45);
+            this.axWindowsMediaPlayer1.TabIndex = 6;
             // 
             // Playlist
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1436, 851);
+            this.ClientSize = new System.Drawing.Size(783, 461);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.btnDeletePlaylist);
+            this.Controls.Add(this.btnDeleteSong);
+            this.Controls.Add(this.btnPlaySong);
+            this.Controls.Add(this.btnAddSong);
+            this.Controls.Add(this.lblPlaylistTitle);
+            this.Controls.Add(this.lstSongs);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Playlist";
             this.Text = "Playlist";
             this.Load += new System.EventHandler(this.Playlist_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstSongs;
+        private System.Windows.Forms.Label lblPlaylistTitle;
+        private System.Windows.Forms.Button btnAddSong;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnPlaySong;
+        private System.Windows.Forms.Button btnDeleteSong;
+        private System.Windows.Forms.Button btnDeletePlaylist;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
